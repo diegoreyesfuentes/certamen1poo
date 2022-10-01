469d3f90bac207ebca02d9eb28b4e5dbfd955856
diegoreyesfuentes <107431871+diegoreyesfuentes@users.noreply.github.com>
2022-09-30 23:01:52 -0300

    Add files via upload

---
+++ IPVG/IPVG/Jefe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPVG
+{
+    public class Jefe
+    {
+        public string rut { get; set; }
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+
+        public Jefe(string rut, string nombre, string apellidos, string direccion, string telefono)
+        {
+            this.rut = rut;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.direccion = direccion;
+            this.telefono = telefono;
+        }
+
+        public Jefe()
+        {
+ 
+
+        }
+
+        
+    }
+}
